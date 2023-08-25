0641e6526a07bc0e2cbcad2f8bd3b682e6d44573
lautaro2427 <142542859+lautaro2427@users.noreply.github.com>
2023-08-25 16:19:38 -0300

    Add files via upload

---
+++ intro ej6.py
@@ -0,0 +1,11 @@
+ckm=int(input("cantidad de km que desea recorrer"))
+
+precio=50
+
+if ckm>=10:
+    precio=50+15*ckm
+    
+else:
+    if ckm<10:
+        precio=50+20*ckm
+print("el precio a pagar es de",precio)            
